2887f9d887e74f18dd07dc1ae54739d733351e91
Agpousa <agustinpousa@gmail.com>
2022-08-08 20:53:54 -0300

    integrando DOM

--- js/class.js
+++ js/class.js
@@ -12,10 +12,25 @@ class habitacion {
 
     }
 
-
+    
 }
 
 let arrayHabitaciones = new Array();
 arrayHabitaciones.push(new habitacion("Doble Estandar",10000));
 arrayHabitaciones.push(new habitacion("Doble Superior",15000));
 arrayHabitaciones.push(new habitacion("Deluxer",25000));
+
+
+function precio(a){
+  return  arrayHabitaciones[a].precio
+}
+
+
+let habitacion1 = document.getElementById("habitacion1")
+let habitacion2 = document.getElementById("habitacion2")
+let habitacion3 = document.getElementById("habitacion3")
+
+habitacion1.innerHTML = "$" + precio(0)
+habitacion2.innerHTML = "$" + precio(1)
+habitacion3.innerHTML = "$" + precio(2)
+
